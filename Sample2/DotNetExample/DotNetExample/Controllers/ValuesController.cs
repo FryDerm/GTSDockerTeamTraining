30c0fb6382c554eb898987ee0ab42a564ca99d61
David Derman <dderman@gatewayticketing.com>
2017-04-07 12:23:13 -0400

    Training materials.

---
+++ Sample2/DotNetExample/DotNetExample/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack.Redis;
+
+namespace DotNetExample.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+			var con = GetRedisConnectionString();
+			var manager = new RedisManagerPool(con);
+
+			using (var client = manager.GetClient())
+			{
+				return client.Get<string>(id.ToString());
+			}
+		}
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+		private string GetRedisConnectionString()
+		{
+			//because of https://github.com/dotnet/corefx/issues/8768
+			var dnsTask = Dns.GetHostAddressesAsync("redis");
+			var addresses = dnsTask.Result;
+			var address = addresses[0].MapToIPv4().ToString();
+
+			return address;
+		}
+	}
+}
